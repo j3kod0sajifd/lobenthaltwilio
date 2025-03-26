bffc00759b061f85e2e34182035668b75d76b4d7
j3kod0sajifd <j.tausendfreund@tausendfreund-media.de>
2025-03-26 13:43:31 +0100

    Update voicemail.js

--- api/voicemail.js
+++ api/voicemail.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(`
     <Response>
-      <Say language="de-DE">Sprechen Sie nach dem Ton.</Say>
-      <Record maxLength="30" playBeep="true" />
+      <Play>https://lobenthalansage-7033.twil.io/Bandansage-Lobenthal.mp3</Play>
+      <Record maxLength="180" transcribe="true" playBeep="true" />
     </Response>
   `);
 }
